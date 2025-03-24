c0f3f6179455d026007874f00380b266eae88f45
Ensar Mehmedovic <84131267+3nsar@users.noreply.github.com>
2025-03-24 15:49:06 +0100

    Updated home.php

--- projectCode/includes/sites/home.php
+++ projectCode/includes/sites/home.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
-    <title>Home</title>
-</head>
+
 <body>
 
     <!--- Hero-Section -->
